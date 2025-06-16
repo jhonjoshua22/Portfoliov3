3bd396c81ed8ea9596f97216049b997a1940dd6f
Jhon Joshua Abutan <sjosh2503@gmail.com>
2025-06-16 21:40:08 +0800

    Delete src/main.js

--- src/main.js
+++
@@ -1,5 +0,0 @@
-import App from './src/App.jsx';
-import { createRoot } from 'https://unpkg.com/react-dom@18/umd/react-dom.development.js';
-
-const root = createRoot(document.getElementById('root'));
-root.render(React.createElement(App));
